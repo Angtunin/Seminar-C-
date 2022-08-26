a99a6e445075bed23097dc6c5962c52daf7bcd54
AleksandrTunin <angtunin@mail.ru>
2022-08-26 16:29:18 +0800

    add Seminar2

---
+++ Seminar2/Task001_RandomMax/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100);
+//Console.WriteLine(number); // вариант вывода ниже 
+int number1 = number / 10;
+int number2 = number % 10;
+int Max = number1; 
+if (number1 < number2)
+{
+    Max = number2;
+}  
+
+Console.WriteLine($"{number}, max число = {Max}");
---
+++ Seminar2/Task002_extraktNum/Program.cs
@@ -0,0 +1,12 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+int number1 = number / 100;
+int number2 = number % 10;
+int numberNew = number1 * 10 + number2;
+
+Console.WriteLine($"{number}, {numberNew}"); 
