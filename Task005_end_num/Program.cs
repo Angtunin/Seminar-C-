e793da179aa8c2c854131374ea8a8c686fb88958
AleksandrTunin <angtunin@mail.ru>
2022-08-26 17:56:51 +0800

    add changes

--- Task005_end_num/Program.cs
+++ Task005_end_num/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Введите число: ");
+﻿// 7. Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 int result = number / 10;
 Console.Write(number - result * 10);
