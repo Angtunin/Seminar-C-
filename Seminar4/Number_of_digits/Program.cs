3fb085362941c8ea5e42902c5a7276444edac0cd
AleksandrTunin <angtunin@mail.ru>
2022-09-08 18:36:10 +0800

    add Seminar4

---
+++ Seminar4/Number_of_digits/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Ndigits(int number)
+{
+    int i = 0;
+    while (number != 0)
+    {
+        i++;
+        number = number / 10;
+    }
+    return i;
+}
+
+Console.WriteLine("Введите число А:");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"{Ndigits(num)}");
---
+++ Seminar4/Product_of_N/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ProductN(int number)
+{
+    int sumN = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        sumN += i;
+    }
+    return sumN;
+}
+
+Console.Write("Введите число: ");
+int numPN = int.Parse(Console.ReadLine());
+Console.WriteLine($"{ProductN(numPN)}");
