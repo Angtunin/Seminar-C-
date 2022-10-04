bb7661ace0054d6386f59e1e6fed74f44843ca83
AleksandrTunin <angtunin@mail.ru>
2022-10-04 14:48:55 +0800

    add Seminar9

---
+++ Seminar9/Task063_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void ShowNumbers(int digit, string linetoPrint)
+{
+    if (digit == 1)
+    {
+        linetoPrint = digit.ToString() + linetoPrint;
+        Console.Write($"{linetoPrint}");
+    }
+    else
+    {
+        linetoPrint = ", " + digit.ToString() + linetoPrint;
+        ShowNumbers(digit - 1, linetoPrint);
+    }
+}
+
+Console.Write("Введите число n: ");
+int digit = int.Parse(Console.ReadLine());
+ShowNumbers(digit, "");
---
+++ Seminar9/Task067/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, 
+// которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumRec(int n, int result = 0)
+{
+if (n != 0)
+{
+result += n % 10 + SumRec(n / 10, result);
+}
+return result;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = SumRec(num);
+Console.WriteLine(sum);
